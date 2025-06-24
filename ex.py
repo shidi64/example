0249f7dec472ccc4ae27c8c5daa134df4b2d1600
sonik <sf.bandurina@mail.ru>
2025-06-24 18:06:51 +0300

    prikolchiki

--- ex.py
+++ ex.py
@@ -1,2 +1,13 @@
+import numpy as np
+import pandas as pd
 def sq(x):
     return x**2
+
+def array(x):
+    mass = []
+    for i in range(1,x+1):
+        temp = [int(num) for num in range(1,x+1)]
+        mass.append(temp)
+    s = np.array(mass)
+    # s =  np.array([[1,2,3],[4,5,6],[7,8,9]])
+    return pd.DataFrame(s)
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from func import func
-
-y = func(53)
-print(y)
+from ex import sq
+from ex import array
+from random import *
+from func import df
+s = array(3)
+datafr = df(10,3)
+print(datafr)
+print()
+print(f'Просто массив 3 на 3')
+print(s)
+print()
